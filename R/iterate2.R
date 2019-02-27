64bc63d0b46da9cb432088cd4832f51b9cf17c69
Jer Boon <jer.booner@gmail.com>
2019-02-27 21:43:09 +0000

    completed a find_influencers function

--- R/iterate2.R
+++ R/iterate2.R
@@ -1,12 +1,42 @@
-no.neighbours <- 10
 influence.radius <- 1
+view.angle <- 135
 
 # Find #s of any birds within the sphere of influence of bird b
 find_influencers <- function(birds,b) {
+  
+  #distance^2 to other birds
   d2 <- (birds$x - birds[b,"x"])^2 + (birds$y - birds[b,"y"])^2 + (birds$z - birds[b,"z"])^2
 
+  #those without sphere of influence^2
   inf <- which(d2 < influence.radius^2)
-  return(inf[inf != b])
+  inf <- inf[inf != b]
+
+  #none found?
+  if(length(inf) == 0)
+    return(NA)
+
+  #fetch data for those, and calculate some other stuff
+  inf.data <- birds[inf,]
+  inf.data$dist <- sqrt((inf.data$x - birds[b,"x"])^2 +
+                        (inf.data$y - birds[b,"y"])^2 +
+                        (inf.data$z - birds[b,"z"])^2 )
+  inf.data$dirx <- inf.data$x - birds[b,"x"]
+  inf.data$diry <- inf.data$y - birds[b,"y"]
+  inf.data$dirz <- inf.data$z - birds[b,"z"]
+  uv <- unit_vector(birds[b,c("dx","dy","dz")])
+  
+  for (i in 1:nrow(inf.data)) {
+    inf.data[i,"angle"] <- sum(uv *
+                               unit_vector(inf.data[i,c("dirx","diry","dirz")]))
+  }
+  inf.data$angle <- acos(inf.data$angle) /2 /pi *360
+  inf.data <- inf.data[inf.data$angle <= view.angle,]
+
+  #none found?
+  if(nrow(inf.data) == 0)
+    return(NA)
+  
+  return(inf.data)
 }
 
 iterate <- function(birds) {
